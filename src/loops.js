598b23ef31f2198eae1913345f0ac6e3013cd0b3
KavyaGowda72 <rskavya7@gmail.com>
2024-08-12 11:05:44 +0530

    Loop Tasks

--- src/loops.js
+++ src/loops.js
@@ -3,10 +3,61 @@
 
 // ex:
 
-outer:for(let i=0;i<3;i++){
-    for(let j=0;j<3;j++){
+// outer:for(let i=0;i<3;i++){
+//     for(let j=0;j<3;j++){
 
-        if(i==2) break
-        console.log(`The Coordinates are : ${i} ${j}`)
-    }
-}
+//         if(i==2) break outer
+//         console.log(`The Coordinates are : ${i} ${j}`)
+//     }
+// }
+
+
+//For Loop:
+// for(let i=2;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+//while Loop
+
+// let i=0;
+// while(i<3){
+//     console.log(i)
+//     i++;
+// }
+
+
+//Task:
+// Output prime numbers
+
+// An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
+
+// In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n.
+
+// For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4.
+
+// Write the code which outputs prime numbers in the interval from 2 to n.
+
+// For n = 10 the result will be 2,3,5,7.
+
+// P.S. The code should work for any n, not be hard-tuned for any fixed value.
+
+
+//Task-2
+
+// Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+// Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+
+// let input;
+// do{
+//    input = prompt('enter a number greater than 100',0)
+
+// }while(input<=100 && input)
+
+
+
+
