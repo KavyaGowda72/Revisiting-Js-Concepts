eddfe99cb6872e54b3d014d123fc0f4ba3ed31dd
KavyaGowda72 <rskavya7@gmail.com>
2024-08-12 19:10:31 +0530

    Arrow Functions

--- src/functions.js
+++ src/functions.js
@@ -70,3 +70,90 @@ power(3,4)
 
 let x=prompt('enter x')
 let n=prompt('enter n')
+
+
+//Function Expression
+
+let sayHi=function(){
+    alert('hello')
+}
+
+//CallBack Function => Function passed as an argument are called allback fun
+
+
+function User(question,yes,no){
+    if(confirm(question)){
+        yes()
+    }  else{
+        no()
+    }
+}
+function myName(){
+    alert('yes it is')
+}
+function notMyName(){
+alert('no it isnt')
+}
+
+User('Is your name kavyaa?', myName,notMyName)
+
+//Funtions without Name=> Anonymous Functions
+
+//Arrow Function
+
+let sum=(a,b) => a+b
+
+alert(sum(3,4))
+
+
+//When there is one argument
+
+let double=n=>n*n   //same as   let double=function(n){return n*n}  
+double(3)
+
+//No parameter
+
+let name1=()=>{    //Parentesis are compulsory when no parameters are present
+
+
+}
+
+//MultiLine statement Arrow Function
+
+
+let addition=(c,d)=>{   //curly braces mandatory
+    let result=c+d
+    return result
+}
+
+alert(addition(4,5))
+
+
+
+//Task  Re-writing below funtion into arrow-func
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes();
+//     else no();
+//   }
+  
+//   ask(
+//     "Do you agree?",
+//     function() { alert("You agreed."); },
+//     function() { alert("You canceled the execution."); }
+//   );
+
+        
+//Solution://
+
+let ask=(question, yes, no)=>{
+    return confirm(question) ? yes() :no()
+}
+
+ask('do you agree' , ()=>alert('agreed'), ()=>alert('canceled'))
+
+
+
+
+
+
