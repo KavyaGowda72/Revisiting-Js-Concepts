8039a95f39ea59e8fe553c1858e723ad6cccd82d
KavyaGowda72 <rskavya7@gmail.com>
2024-08-12 14:43:53 +0530

    switch

---
+++ src/switchStatement.js
@@ -0,0 +1,29 @@
+// let x='Monday'
+
+// switch(x){
+//     case 'Hey':
+//     break;
+//     case 'Monday':
+//         console.log('Exactly')
+//         break;
+//     default: 'Enter correct day'
+//     break
+// }
+
+
+//Tasks:
+
+let a = +prompt('a?', '');
+
+switch(a){
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1)
+        break
+    case 2 :
+    case  3:
+        alert('2,3')
+        break;
+}
